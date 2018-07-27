755c032edc811d6d48f5fa292bb65b3b9d6737e3
Rachid Benshila <rblod@locean-ipsl.upmc.fr>
2018-07-27 11:44:28 +0200

    blba

--- Scripts/create_config_ensemble.sh
+++ Scripts/create_config_ensemble.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
 . $(cd $(dirname "$0")/..; pwd)/set_path.sh
 
-[ ! -d ${WORKDIR} ] && mkdir ${WORKDIR}
+
+echo "Creating ${WORKDIR} and ${CASEDIR}${mem} structure"
+echo '--------------------------------------------------'
+echo
+\rm -rf ${WORKDIR}
+mkdir ${WORKDIR}
 cd ${WORKDIR}
 
 for i in `seq ${ID_BEG} ${ENSSIZE}`
@@ -10,23 +14,26 @@ do
    mem=`echo 00$i | tail -4c`
    
    [ ! -d ${CASEDIR}${mem} ] && mkdir ${CASEDIR}${mem}
-   cd ${CASEDIR}${mem}
-   
-   
-   # model
-#   \cp ${EXECDIR}/${exec} .
-#   cp ${EXECDIR}namelist .
-#   ln -fs ${EXECDIR}/namelist .
-   
-   
-#   cp ${EXECDIR}input_param.txt .
-# change here for rel obs path
-#   cp   ${OBSDIR}/shoreface_out_00.nc shoreface_out.nc
-#   \cp   ../../WKB_MODEL/shoreface_out.nc .
-   
-#   cp ARCHIVE/shoreface_forecast${mem}_1.nc shoreface_out.nc
-   
-#   mkdir ARCHIVE
-   
-   cd ${WORKDIR}
 done
+
+echo "Cleaning ${ASSIMDIR}"
+echo '--------------------------------------------------'
+echo
+\rm ${ASSIMDIR}/analysis???.nc
+\rm ${ASSIMDIR}/forecast???.nc
+\rm ${ASSIMDIR}/log_assim_*.txt
+
+
+echo "Initial perturbation of ${EXECDIR}/shoreface_out.nc"
+echo '---------------------------------------------------'
+echo
+cd ${WORKDIR}
+cp ${EXECDIR}/shoreface_out.nc .
+python ../PREPRO/genebathy.py ${ENSSIZE} shoreface_out.nc ${ENSSIZE} 0
+for i in `seq ${ID_BEG} ${ENSSIZE}`
+do
+     mem=`echo 00$i | tail -4c`
+     \mv bathy_${mem}.nc ${CASEDIR}${mem}/shoreface_out.nc
+     cd ${WORKDIR}
+done
+\rm shoreface_out.nc
